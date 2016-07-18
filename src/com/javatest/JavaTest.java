a922fab96cc69b471f3e0e98897867597c5605df
MrSourcePlex <lars.thomae@aol.com>
2016-07-18 23:20:20 +0200

    added console output

--- src/com/javatest/JavaTest.java
+++ src/com/javatest/JavaTest.java
@@ -2,7 +2,11 @@ package com.javatest;
 
 public class JavaTest {
 	
+	public JavaTest() {
+		System.out.println("JavaTest");
+	}
+	
 	public static void main(String[] args) {
-		
+		new JavaTest();
 	}
 }
