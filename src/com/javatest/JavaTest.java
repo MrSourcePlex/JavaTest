5896c21dce12dc7bbd65c76c885b34f06afb1169
MrSourcePlex <lars.thomae@aol.com>
2016-07-18 23:36:05 +0200

    Changed the output

--- src/com/javatest/JavaTest.java
+++ src/com/javatest/JavaTest.java
@@ -3,7 +3,7 @@ package com.javatest;
 public class JavaTest {
 	
 	public JavaTest() {
-		System.out.println("JavaTest");
+		System.out.println("Thats a Java Test for GitHub");
 	}
 	
 	public static void main(String[] args) {
